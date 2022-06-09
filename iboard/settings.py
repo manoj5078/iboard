d84bcfac393b2e4daf82d3e2f1f05dd2c8cbe29c
Manoj Tolagekar <51877124+manoj5078@users.noreply.github.com>
2022-06-09 16:56:01 +0530

    Update settings.py

--- iboard/settings.py
+++ iboard/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8kkx8n+nzfr0fg65gb@2f!i@nno7882$_p28fvz7-mef6h-y&s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
